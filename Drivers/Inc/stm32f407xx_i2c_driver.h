06fe7d4fb130bd3d0b2a6ae953d4f8ac42dc5f50
manigandan-d <manigandan220916@gmail.com>
2024-05-26 17:12:06 +0530

    i2c driver header

--- Drivers/Inc/stm32f407xx_i2c_driver.h
+++ Drivers/Inc/stm32f407xx_i2c_driver.h
@@ -1,10 +1,3 @@
-/*
- * stm32f407xx_i2c_driver.h
- *
- *  Created on: Jan 20, 2024
- *      Author: MANIGANDAN D
- */
-
 #ifndef INC_STM32F407XX_I2C_DRIVER_H_
 #define INC_STM32F407XX_I2C_DRIVER_H_
 
